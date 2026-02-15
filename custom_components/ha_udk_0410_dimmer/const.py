aedd59fb793dbdc845f971e32cfedeec63c2e999
cnc-lasercraft <96776729+cnc-lasercraft@users.noreply.github.com>
2026-02-15 09:12:46 +0100

    Create const.py

---
+++ custom_components/ha_udk_0410_dimmer/const.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+DOMAIN = "udk_0410_dimmer"
+
+CONF_PORT = "port"
+CONF_BAUDRATE = "baudrate"
+
+CONF_MODULES = "modules"  # stored in entry.options
+# Module dict keys
+MOD_NAME = "name"
+MOD_ADDRESS = "address"
+MOD_DIMMERS = "dimmers"  # list of 4 dicts {index, name}
